e5667c9fdf00ff30bead18b40eeb58d8e87db80e
KimSesame <kimbs082787@gmail.com>
2024-10-10 17:11:02 +0900

    feat: Visualize AR brush

---
+++ Assets/Scripts/ArBrush.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArBrush : MonoBehaviour
+{
+    [Header("Color")]
+    [SerializeField] Color selectedColor;
+
+    private SpriteRenderer spRenderer;
+
+    private void Awake()
+    {
+        spRenderer = GetComponent<SpriteRenderer>();
+
+        SelectColor(Color.black);
+    }
+
+    public void ToggleBrush()
+    {
+        spRenderer.enabled = GameManager.Instance.curPainter.Equals(GameManager.Instance.arPainter);
+    }
+
+    public void SelectColor(Color color)
+    {
+        selectedColor = color;
+        spRenderer.color = selectedColor;
+    }
+}
