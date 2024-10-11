d6e32a26b589dbb23ea0f03a10ab7ef2d9f58e81
KimSesame <kimbs082787@gmail.com>
2024-10-11 18:08:32 +0900

    feat: Magnify color palette when the palette is selected

--- Assets/Scripts/ColorPicker.cs
+++ Assets/Scripts/ColorPicker.cs
@@ -5,14 +5,69 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ColorPicker : MonoBehaviour, IPointerDownHandler, IDragHandler
+public class ColorPicker : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
     public UnityEvent<Color> OnColorPicked;
 
     public Color color;
 
+    [SerializeField] Image paletteThumbnail; 
     [SerializeField] Image palette; 
     [SerializeField] Image picker;
+
+    private Image paletteImage;
+    private BoxCollider2D paletteCollider;
+    private Image paletteThumbnailImage;
+    private BoxCollider2D paletteThumbnailCollider;
+
+    private void Awake()
+    {
+        paletteImage = palette.GetComponent<Image>();
+        paletteCollider = palette.GetComponent<BoxCollider2D>();
+        paletteThumbnailImage = paletteThumbnail.GetComponent<Image>();
+        paletteThumbnailCollider = paletteThumbnail.GetComponent<BoxCollider2D>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        MagnifyPalette(true);
+        SelectColor();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        SelectColor();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        MagnifyPalette(false);
+        RepositionPicker();
+    }
+
+    private void MagnifyPalette(bool flag)
+    {
+        paletteImage.enabled = flag;
+        paletteCollider.enabled = flag;
+        paletteThumbnailImage.enabled = !flag;
+        paletteThumbnailCollider.enabled = !flag;
+    }
+
+    private void RepositionPicker()
+    {
+        RectTransform paletteRect = palette.GetComponent<RectTransform>();
+        RectTransform paletteThumbnailRect = paletteThumbnail.GetComponent<RectTransform>();
+        Vector2 localPoint = picker.rectTransform.anchoredPosition;
+
+        // Calculate RectTransfrom coordinate
+        float offsetX = paletteRect.rect.width / 2 - paletteThumbnailRect.rect.width / 2;
+        float offsetY = (-paletteRect.rect.height / 2) + paletteThumbnailRect.rect.height / 2;
+        float newLocalX = localPoint.x * paletteThumbnailRect.rect.width / paletteRect.rect.width + offsetX;
+        float newLocalY = localPoint.y * paletteThumbnailRect.rect.height / paletteRect.rect.height + offsetY;
+
+        // Reposition
+        picker.rectTransform.anchoredPosition = new Vector2(newLocalX, newLocalY);
+    }    
 
     private void SelectColor()
     {
@@ -44,21 +99,11 @@ public class ColorPicker : MonoBehaviour, IPointerDownHandler, IDragHandler
         // Calculate texture coordinate
         float ratioX = (localPoint.x + paletteRect.rect.width / 2) / paletteRect.rect.width;
         float ratioY = (localPoint.y + paletteRect.rect.height / 2) / paletteRect.rect.height;
-        int textureX = Mathf.RoundToInt(ratioX * textureRect.width + textureRect.x);
-        int textureY = Mathf.RoundToInt(ratioY * textureRect.height + textureRect.y);
+        int textureX = Mathf.RoundToInt(ratioX * textureRect.width);
+        int textureY = Mathf.RoundToInt(ratioY * textureRect.height);
 
         pickedColor = paletteTexture.GetPixel(textureX, textureY);
 
         return pickedColor;
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        SelectColor();
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        SelectColor();
     }
 }
